72fc36c478a94e7cf192d42fc9100cdb2aa61c13
seedevk8s <seedevk8s@gmail.com>
2024-05-01 16:21:28 +0000

    JIB 플러그인을 사용하여 Docker 이미지 생성하기

--- src/main/java/com/kosta/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/kosta/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -16,7 +16,7 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldBean() {
 		// throw new RuntimeException("Some Error has Happened! Contact Support at
 		// ***-***");
-		return new HelloWorldBean("Hello World v4");
+		return new HelloWorldBean("Hello World v5");
 	}
 
 	/// hello-world/path-variable/in28minutes
